47779b03e8c5c16768abda45a862edc71d419845
SLIDE47 <jufejxn@163.com>
2023-05-02 02:48:01 -0400

    Update views.py

--- playground/views.py
+++ playground/views.py
@@ -17,4 +17,4 @@ class HelloView(APIView):
             data = response.json()
         except requests.ConnectionError:
             logger.critical('httpbin is offline')
-        return render(request, 'hello.html', {'name': 'Mosh'})
+        return render(request, 'hello.html', {'name': 'Xingnan Jiang'})
